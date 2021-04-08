ba96a01d7190b6e39850b1ea86302567359cd69d
Adigun Jide Idris <edijman@gmail.com>
2021-04-08 09:26:17 -0400

    Styled board

--- src/suduko/components/Tiles.js
+++ src/suduko/components/Tiles.js
@@ -2,14 +2,14 @@ import React from 'react'
 import './Tiles.css'
 const Tiles = ({tile, changeTile}) => {
     return (
-        <div>
-           <input className="tiles"
+        <>
+           <input className={`tiles + col-${tile.col} + row-${tile.row}`}
             value={tile.value ? tile.value: ""} 
             readOnly={tile.readonly} 
             type="text"
             onChange={(e) => changeTile(tile.index, e.target.value)}
             maxLength="1"/>
-        </div>
+        </>
     )
 }
 
